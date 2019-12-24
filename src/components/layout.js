123e05f589eefca1837737fc784d3288f1978117
Sam Slotsky <sam@manifold.co>
2019-12-24 10:39:25 -0600

    Add helmet provider

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,15 +5,16 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
+import { HelmetProvider } from "react-helmet-async";
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header";
+import "./layout.css";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <HelmetProvider>
       <Header />
       <div
         style={{
@@ -30,12 +31,12 @@ const Layout = ({ children }) => {
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
-    </>
-  )
-}
+    </HelmetProvider>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
