cd7d9eb310ec31761463110a0172fde9ee2d2be5
Sam Slotsky <sam@manifold.co>
2019-12-24 10:35:54 -0600

    Switch SEO component to TS

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -6,11 +6,22 @@
  */
 
 import React from "react";
-import PropTypes from "prop-types";
 import { Helmet } from "react-helmet-async";
 import { useStaticQuery, graphql } from "gatsby";
 
-function SEO({ description, lang, meta, title }) {
+interface SEOProps {
+  description?: string;
+  lang?: string;
+  meta: object[];
+  title: string;
+}
+
+function SEO({
+  description,
+  lang = "en",
+  meta = [],
+  title,
+}): React.ReactElement<SEOProps> {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -71,18 +82,5 @@ function SEO({ description, lang, meta, title }) {
     />
   );
 }
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-};
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-};
 
 export default SEO;
