724b428a29092dbe30d307685d19a6c5d314f5f3
Sam Slotsky <sam@manifold.co>
2019-12-26 11:07:29 -0600

    Haiku and background on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,47 @@
-import React from "react"
+import React from "react";
+import { styled } from "linaria/react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const Haiku = styled.p`
+  font-style: italic;
+  text-indent: -1.5rem;
+  margin-left: 1.5rem;
+  color: white;
+`;
+
+const Background = styled.div`
+  background: url(../images/desolation.jpg);
+  background-size: cover;
+  height: 100vh;
+  border-radius: 0.5rem;
+  position: relative;
+`;
+
+const Backdrop = styled.div`
+  background: linear-gradient(0deg, #000000 0%, rgba(0, 0, 0, 0) 100%);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding: 2rem;
+`;
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Background>
+      <Backdrop>
+        <Haiku>
+          Wind catches lily
+          <br /> Scatt'ring petals to the wind:
+          <br /> Your site is not found.
+        </Haiku>
+      </Backdrop>
+    </Background>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
