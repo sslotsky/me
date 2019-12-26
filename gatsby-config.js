99476f52e6a1c52c96f59706584a7be9e3efdb77
Sam Slotsky <sam@manifold.co>
2019-12-25 18:28:47 -0600

    First blog post

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [`gatsby-remark-prismjs`],
---
+++ src/pages/blog.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { graphql } from "gatsby";
+import { styled } from "linaria/react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const TitleHeading = styled.h2`
+  text-align: center;
+`;
+
+const Title = ({ path, children }) => (
+  <Link
+    to={path}
+    style={{
+      textDecoration: "none",
+    }}
+  >
+    <TitleHeading>{children}</TitleHeading>
+  </Link>
+);
+
+const Bottom = styled.div`
+  position: absolute;
+  bottom: 0em;
+  width: 100%;
+  height: 4rem;
+  background-image: linear-gradient(
+    rgba(255, 255, 255, 0) 0%,
+    rgba(255, 255, 255, 1) 100%
+  );
+`;
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const BlogIndex = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges.map(({ node: { id, frontmatter, excerpt } }) => (
+    <Container key={id}>
+      <Title path={frontmatter.path}>{frontmatter.title}</Title>
+      <p dangerouslySetInnerHTML={{ __html: excerpt }} />
+      <Bottom />
+    </Container>
+  ));
+
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      {Posts}
+    </Layout>
+  );
+};
+
+export default BlogIndex;
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { fileAbsolutePath: { regex: "/blog/" } }
+    ) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 450)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`;
